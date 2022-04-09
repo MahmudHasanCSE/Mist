6934d44a35bd3eed7ff69eebab9be27679d4ad32
zero-fighter <rauaha@gmail.com>
2022-04-09 20:23:09 +0600

    initial commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('admin.category.add');
+    }
+
+    public function create(Request $request)
+    {
+        Category::newCategory($request);
+        return redirect()->back()->with('message', 'Category Created Successfully!');
+    }
+
+    public function manage()
+    {
+        return view('admin.category.manage');
+    }
+}
+
+
